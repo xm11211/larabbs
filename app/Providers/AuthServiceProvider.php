27e6b980bfd51e593356fc3fa62686e409a49bdf
xm11211 <980172892@qq.com>
2018-10-12 06:41:23 +0000

    Generated topics

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -15,6 +15,7 @@ class AuthServiceProvider extends ServiceProvider
 
     //对授权策略进行注册
     protected $policies = [
+		 \App\Models\Topic::class => \App\Policies\TopicPolicy::class,
         'App\Model' => 'App\Policies\ModelPolicy',
         \App\Models\User::class  => \App\Policies\UserPolicy::class,
     ];
